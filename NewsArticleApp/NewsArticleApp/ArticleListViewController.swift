9701d5bb2ce0c028fa901e90acdaf56fbf34f25e
Mohsin <mohammad.mohsin>
2024-11-10 19:43:24 +0530

    feat: added font + controller

---
+++ NewsArticleApp/NewsArticleApp/ArticleListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  NewsArticleApp
+//
+//  Created by Mohammad Mohsin on 10/11/24.
+//
+
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var searchIconView: UIImageView!
+    @IBOutlet weak var articleListContainerView: UIView!
+    @IBOutlet weak var articleListTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configTheme()
+    }
+}
+
+extension ArticleListViewController {
+
+    private func configTheme() {
+        titleLabel.textColor = UIColor(named: "#000000")
+        titleLabel.font = UIFont(name: "Roboto-Bold", size: 30)
+        titleLabel.text = "Articles"
+        searchIconView.image = UIImage(named: "searchIcon")
+    }
+}
